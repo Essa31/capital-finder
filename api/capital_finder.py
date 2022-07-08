818e7e576ee81f20e76c6b8d71133ba385d4a8eb
Essa31 <esaaeyad22@gmail.com>
2022-07-08 14:48:00 -0700

    capital-finder

--- api/capital_finder.py
+++ api/capital_finder.py
@@ -1,4 +1,4 @@
-from http.server import BaseHTTPRequestHandler
+from http.server import BaseHTTPRequestHandler, HTTPServer
 from urllib import parse
 import requests
 
@@ -8,22 +8,23 @@ class Handler(BaseHTTPRequestHandler):
         url_components = parse.urlsplit(self.path)
         query_string_list = parse.parse_qsl(url_components.query)
         dictionary = dict(query_string_list)
-        base_url = "https://restcountries.com/v2/"
-        capital = dictionary.get("capital")
-        country = dictionary.get("country")
 
-        if country:
-            response = requests.get(base_url +  country)
+        if 'country' in dictionary:
+            country = dictionary['country']
+            url = 'https://restcountries.com/v3.1/name/'
+            response = requests.get(url + country)
             data = response.json()
             capital_response = data[0]["capital"]
-            message = f"The capital of {country} is {capital_response}"
+            message = f"The capital of " + str(country) + " is " + str(capital_response)
 
-        elif capital:
-            response = requests.get(base_url +  capital)
+        elif 'capital' in dictionary:
+            capital = dictionary['capital']
+            url = 'https://restcountries.com/v2/capital/'
+            response = requests.get(url + capital)
             data = response.json()
             capitals = data[0]["capital"]
             country_name = data[0]["name"]
-            message = f"{capitals} is the capital of {country_name}"
+            message = str(capitals) + " is " + "the capital of " + str(country_name)
 
         else:
             message = "Please write a city name to get info about it"
@@ -36,5 +37,13 @@ class Handler(BaseHTTPRequestHandler):
 
         return
 
+def main():
+    port = 8000
+    server_address = ('localhost', port)
+    server = HTTPServer(server_address, Handler)
+    print(f'Server is running')
+    server.serve_forever()
 
 
+if __name__ == "__main__":
+    main()
