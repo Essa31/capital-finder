e960e69816c97564da8ed7525894bd7747207f41
Essa31 <esaaeyad22@gmail.com>
2022-07-08 18:43:31 -0700

    capital-finder

--- api/capital_finder.py
+++ api/capital_finder.py
@@ -4,36 +4,35 @@ import requests
 
 
 class handler(BaseHTTPRequestHandler):
+
     def do_GET(self):
-        path=self.path
-        url_components = parse.urlsplit(path)
+        s = self.path
+        url_components = parse.urlsplit(s)
         query_string_list = parse.parse_qsl(url_components.query)
-        dictionary = dict(query_string_list)
+        dic = dict(query_string_list)
 
-        if 'country' in dictionary:
-            country = dictionary['country']
+        if 'country' in dic:
+            country = dic['country']
             url = 'https://restcountries.com/v3.1/name/'
-            response = requests.get(url + country)
-            data = response.json()
-            capital_response = data[0]["capital"][0]
-            message = f"The capital of " + str(country) + " is " + str(capital_response)
+            r = requests.get(url + country)
+            data = r.json()
 
-        elif 'capital' in dictionary:
-            capital = dictionary['capital']
+            the_name = data[0]["capital"][0]
+            message = "The capital of " + str(country) + " is " + str(the_name)
+        elif 'capital' in dic:
+            capital = dic['capital']
             url = 'https://restcountries.com/v2/capital/'
-            response = requests.get(url + capital)
-            data = response.json()
+            r = requests.get(url + capital)
+            data = r.json()
+
             the_name = data[0]["name"]
             message = str(capital) + " is the capital of " + str(the_name)
-
-
         else:
-            message = "Please write a city name to get info about it"
+            message = "Make sure you right ?capital=example  or ?country=example  is correct !!!"
 
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
-
         self.wfile.write(message.encode())
 
         return
